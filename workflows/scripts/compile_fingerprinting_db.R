9124747b94b9f25343127b191160ece1e16aeaea
millergw <gmiller@broadinstitute.org>
2019-12-20 10:20:21 -0800

    edited FNG db compiler
    alter FNG db compiler so that it merges newly discovered FNGs with the previous version of the FNG db instead of recompiling the whole db every time; this change means we can implement a cleaner version of the workflow that will always have the most complete version of the FNG db even if the current workspace doesn't have all the samples.
    
    For example, if the workspace just has TWIST samples, this method allows for the FNG db to also contain the FNG results from TSCA.

--- workflows/scripts/compile_fingerprinting_db.R
+++ workflows/scripts/compile_fingerprinting_db.R
@@ -1,6 +1,7 @@
 ############################################
 # Compute pileup frequencies for multiple samples, creating fingerprinting DB
 # mcadosch 10/17
+# Gwen Miller edited 12/2019
 ############################################
 
 rm(list=ls())
@@ -22,11 +23,17 @@ parser$add_argument('--pileup_pct_count_paths', type="character", nargs="+",
                    help='pileup_pct_count files', required=T)
 parser$add_argument('--samples_tsca_ids', type="character", nargs="+", 
 					help='sample tsca_id', required=T)
+parser$add_argument('--prev_fng_db_path', type="character", nargs="+", 
+                    help='previous FNG database to merge new FNG data into', required=T)
 
 args <- parser$parse_args()
 files <- args$pileup_pct_count_paths
 samples_tsca_ids <- args$samples_tsca_ids
+prev_fng_db <- args$prev_fng_db_path
 
+##########
+## Compile table for all new samples
+##########
 # Number of samples
 N = length(files)
 samples_data <- vector("list", N)
@@ -38,6 +45,26 @@ for (i in seq_along(files)) {
 	samples_data[[i]] <- sample_data
 }
 
-my_db <- samples_data %>% bind_rows()
+new_fngs <- samples_data %>% bind_rows()
+
+##########
+## Merge new FNG db with the previously existing FNG db
+##########
+# read in previous FNG db
+prev_fng_db <- read.delim(prev_fng_db)
+
+# they should share the same columns
+if (colnames(prev_fng_db) != colnames(new_fngs)){
+  print("Stopping execution: the column names for the old and new FNG db do not match!")
+}
+# stop running if they do no not share the same column names
+stopifnot(colnames(prev_fng_db) == colnames(new_fngs))
+
+# merge and delete duplicate rows (no dups found in this instance)
+no_dups_merged <- bind_rows(new_fngs, prev_fng_db) %>% distinct(merged)
+
+##########
+## write out the final compiled FNG db
+##########
 output = paste0("fingerprinting_db.txt")
-write.delim(my_db, output)
+write.delim(no_dups_merged, output)
